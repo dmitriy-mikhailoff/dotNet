73618e47516478a5b9f8281aee78a2b17ff0c6b9
Dmitriy Mikhailoff <dmitriy.mikhailoff@gmail.com>
2021-05-25 23:13:42 +0300

    added new class

---
+++ Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pj1
+{
+    class Class1
+    {
+        public void greeting()
+        {
+            Console.WriteLine("Hi");
+        }
+    }
+}
